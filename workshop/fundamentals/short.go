5ad0c79f10c2f1b0e16dc23d3dad4ad6580704dd
Dineshraj Goomany <dineshraj.goomany@bbc.co.uk>
2020-01-24 15:26:12 +0000

    short variable declarations

--- workshop/fundamentals/short.go
+++ workshop/fundamentals/short.go
@@ -15,11 +15,11 @@ func Convert(n ...float64) []float64 {
 
 // ShortVars ...
 func ShortVars() (float64, float64, float64) {
-	var boilingF float64 = 212.0
-	var freezingF float64 = 32.0
-	var absoluteZeroF float64 = -459.67
+	boilingF := 212.0
+	freezingF := 32.0
+	absoluteZeroF := -459.67
 
-	var celsius []float64 = Convert(boilingF, freezingF, absoluteZeroF)
+	celsius := []float64(Convert(boilingF, freezingF, absoluteZeroF))
 
 	return celsius[0], celsius[1], celsius[2]
 }
